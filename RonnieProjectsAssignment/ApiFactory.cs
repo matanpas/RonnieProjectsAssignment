5edaa39628c6fdf895a4f183bf2660c95dfe0bb6
matanpas <matan.pas@gmail.com>
2025-06-05 19:16:50 +0300

    prettier cosmetics

--- RonnieProjectsAssignment/ApiFactory.cs
+++ RonnieProjectsAssignment/ApiFactory.cs
@@ -2,22 +2,14 @@
 
 public static class ApiFactory
 {
-    private static readonly string[] Urls =
-    [
-        "https://randomuser.me/api/?results=500",
-        "https://jsonplaceholder.typicode.com/users",
-        "https://dummyjson.com/users",
-        "https://reqres.in/api/users"
-    ];
-
     public static Dictionary<string, IApiParser> GetDictionary()
     {
         return new Dictionary<string, IApiParser>
         {
-            [Urls[0]] = new ApiParser1(),
-            [Urls[1]] = new ApiParser2(),
-            [Urls[2]] = new ApiParser3(),
-            [Urls[3]] = new ApiParser4()
+            ["https://randomuser.me/api/?results=500"] = new ApiParser1(),
+            ["https://jsonplaceholder.typicode.com/users"] = new ApiParser2(),
+            ["https://dummyjson.com/users"] = new ApiParser3(),
+            ["https://reqres.in/api/users"] = new ApiParser4()
         };
     }
 }
